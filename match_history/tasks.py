dc27bdadd781bf8e1c53f0b09e81ae1f9369994a
framarqu001 <119390184+framarqu001@users.noreply.github.com>
2024-08-24 20:39:51 -0400

    added celery back ground task and progress bar to parse a summoenr completely

---
+++ match_history/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+from django.core.cache import cache
+
+from populate_data import Summoner, MatchManager
+from django.core.exceptions import ObjectDoesNotExist
+from celery_progress.backend import ProgressRecorder
+
+
+@shared_task(bind=True)
+def process_matches(self, summoner_id):
+    try:
+        progress_recorder = ProgressRecorder(self)
+        summoner = Summoner.objects.get(puuid=summoner_id)
+        match_builder = MatchManager("americas", "na1", summoner)
+        match_builder.process_matches(progress_recorder=progress_recorder)
+    except Summoner.DoesNotExist:
+        print(f"Summoner with id {summoner_id} does not exist")
+    finally:
+        summoner.being_parsed = False
+        summoner.save()
